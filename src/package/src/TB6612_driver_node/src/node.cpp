4a6bbb8c41228cd098a696a69af376ce93874e5d
root <root@ubilinux.(none)>
2016-02-21 03:05:15 +0000

    got driver node working

--- src/package/src/TB6612_driver_node/src/node.cpp
+++ src/package/src/TB6612_driver_node/src/node.cpp
@@ -2,10 +2,14 @@
 #include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
 #include <sstream>
+#include "SparkFun_TB6612_Edison.h"
 
 void driveCallback(const geometry_msgs::TwistConstPtr &input)
 {
     ROS_INFO("driving");
+    tb6612 motors;
+    motors.standby(false);
+    motors.diffDrive(input->linear.x, input->linear.x);    
 }
 
 int main(int argc, char **argv)
